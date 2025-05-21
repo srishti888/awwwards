be1e57d9019917b402157f5f389aa10940f43be9
srishti888 <152876543+srishti888@users.noreply.github.com>
2025-05-21 17:54:00 +0530

    just started

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          50: '#dfdff0',
+          75: '#dfdff2',
+          100: '#dfdff5',
+          200: '#dfdff0',
+          300: '#4fb7dd',
+        },
+        violet: {
+          700: '#7008e7',
+        },
+        yellow: {
+          100: '#8e983f',
+          300: '#edff66'
+        }
+      }
+    }
+  }
+})
